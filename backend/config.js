f73ed4b6ed084d065bfc915241a67cf78360381c
richhhmz <richhhmz@users.noreply.github.com>
2026-02-18 12:47:06 -0700

    fix frontend port

--- backend/config.js
+++ backend/config.js
@@ -45,6 +45,6 @@ export const SENDGRID_API_KEY   = process.env.SENDGRID_API_KEY;
 export const SENDGRID_FROM_EMAIL = process.env.SENDGRID_FROM_EMAIL;
 
 export const FRONTEND_ORIGIN =
-  process.env.FRONTEND_ORIGIN || 'http://localhost:8080';
+  process.env.FRONTEND_ORIGIN || 'http://localhost:5173';
 
 export const isProd = process.env.NODE_ENV === 'production';
